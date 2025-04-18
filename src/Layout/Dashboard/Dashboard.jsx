6c85e266fe34dbc8b5f5a940b58d0b798e97b42d
zillulofficial <zillul.hakim100@gmail.com>
2025-04-18 20:06:13 +0600

    dashboard bugs fixed for smaller devices

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -39,7 +39,7 @@ const Dashboard = () => {
 
                                             <span className="mx-2 text-sm font-medium">Users</span>
                                         </NavLink>
-                                        <NavLink to='dashboard/contacts' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
+                                        <NavLink to='/dashboard/contacts' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
                                             "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg hover:bg-[#0066ff10] cursor-pointer"}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
@@ -52,13 +52,13 @@ const Dashboard = () => {
                                     <div className="space-y-3 ">
                                         <label className="px-3 text-xs text-gray-500 uppercase">content</label>
 
-                                        <NavLink to='/dashboard/addItems' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
+                                        <NavLink to='/dashboard/addItem' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
                                             "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg hover:bg-[#0066ff10] cursor-pointer"}>
                                             <MdAddChart className="text-xl"/>
 
                                             <span className="mx-2 text-sm font-medium">Add Item</span>
                                         </NavLink>
-                                        <NavLink to='/dashboard/items' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
+                                        <NavLink to='/dashboard/item' className={({ isActive }) => isActive ? "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg bg-[#0066ff10] cursor-pointer" :
                                             "flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg hover:bg-[#0066ff10] cursor-pointer"}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
@@ -66,11 +66,7 @@ const Dashboard = () => {
 
                                             <span className="mx-2 text-sm font-medium">Item List</span>
                                         </NavLink>
-
-
                                     </div>
-
-                                    
                                 </nav>
                             </div>
                         </aside>
